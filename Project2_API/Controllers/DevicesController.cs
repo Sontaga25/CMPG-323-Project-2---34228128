f649e944b6e151265dda6859768db706870ca67a
Sontaga <foraneg@gmail.com>
2022-09-08 13:38:31 +0200

    final product

--- Project2_API/Controllers/DevicesController.cs
+++ Project2_API/Controllers/DevicesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,7 @@ using Project2_API.Models;
 
 namespace Project2_API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class DevicesController : ControllerBase
